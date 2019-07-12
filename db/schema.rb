b9fe37a616cd6e9217960d64c22fe7b059fdeb2b
Donna Amos <donnacamos.developer@gmail.com>
2019-07-12 19:10:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190712185402) do
+ActiveRecord::Schema.define(version: 20190712190850) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
