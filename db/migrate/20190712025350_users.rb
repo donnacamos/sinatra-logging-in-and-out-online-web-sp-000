094bd8bca4adac9e7db8837a4d71b653f08c7fc3
Donna Amos <donnacamos.developer@gmail.com>
2019-07-12 18:41:44 +0000

    Automatically backed up by Learn

--- db/migrate/20190712025350_users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ActiveRecord::Migration[5.1]
-  def change 
-    create_table :users do |t| 
-      t.string :username 
-      t.string :password 
-      t.string :balance 
-    end 
-  end
-end
